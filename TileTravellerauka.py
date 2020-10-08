6342102bf45a814de7285c55e02e22893e13da6e
arnardecay <arnarj19@ru.is>
2020-10-08 11:01:20 +0000

    Updated lever option

--- TileTravellerauka.py
+++ TileTravellerauka.py
@@ -4,18 +4,24 @@ valid_input = str()
 x_loc = 1
 y_loc = 1
 lever_tuple = [(1,2),(2,2),(2,3),(3,2)]
+coint_count = 0
 
-def lever():
-    coint_count = 0
-    lever = input("Pull a lever (y/n): ")
-    if lever == "y" or lever == "Y":
-        coint_count += 1
-        print("You received 1 coin, your total is now.", coint_count)
-    elif lever == "n" or lever == "N":
-        continue
 
-def checking_lever(current_tile):
-    pass
+def lever(coint_count):
+    while True:
+        lever = str(input("Pull a lever (y/n): "))
+        lever = lever.lower()
+        if not lever in 'yn':
+            print("Invalid input")
+            continue
+        if lever == 'y':
+            coint_count += 1
+            print(f"You received 1 coins, your total is now {coint_count}.")
+            break
+        elif lever == 'n':
+            break
+    return coint_count
+
 
 
 
@@ -26,8 +32,9 @@ def current_tile(x_loc, y_loc):
         valid_input = "n", "N"
 
     elif x_loc == 1 and y_loc == 2:
+        pull_lever = lever(coint_count)
         print("You can travel: (N)orth or (E)ast or (S)outh.")
-        valid_input = "N", "E", "S"
+        valid_input = "N", "E", "S"  
 
     elif x_loc == 1 and y_loc == 3:
         print("You can travel: (E)ast or (S)outh.")
@@ -38,10 +45,12 @@ def current_tile(x_loc, y_loc):
         valid_input = "N"
 
     elif x_loc == 2 and y_loc == 2:
+        pull_lever = lever(total_coins)
         print("You can travel: (S)outh or (W)est.")
         valid_input = "S", "W"
 
     elif x_loc == 2 and y_loc == 3:
+        pull_lever = lever(total_coins)
         print("You can travel: (E)ast or (W)est.")
         valid_input = "E", "W"
 
@@ -51,6 +60,7 @@ def current_tile(x_loc, y_loc):
         victory = True
 
     elif x_loc == 3 and y_loc == 2:
+        pull_lever = lever(total_coins)
         print("You can travel: (N)orth or (S)outh.")
         valid_input = "N", "S"
 
